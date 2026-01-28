e87fe38776d24469d4eef2f518234cd5c2672454
Ahmed Arab <me@ahmedarab.ly>
2026-01-28 10:54:13 +0200

    Contact us section

---
+++ resources/views/components/contact-form.blade.php
@@ -0,0 +1,10 @@
+<div class="bg-white rounded pt-10 px-16 pb-4 grid gap-y-6 justify-items-center items-start content-start">
+    <input type="text" name="name" value=""
+        class="appearance-none bg-[#f1f1f1] placeholder:text-[#475057] border-0 p-4 rounded shadow" placeholder="الاسم" />
+    <input type="text" name="name" value=""
+        class="appearance-none bg-[#f1f1f1] placeholder:text-[#475057] border-0 p-4 rounded shadow"
+        placeholder="عنوان الرسالة" />
+    <textarea placeholder="الرسالة" rows="2" cols=""
+        class="appearance-none bg-[#f1f1f1] placeholder:text-[#475057] border-0 p-4 rounded shadow"></textarea>
+    <button type="submit" class="bg-primary text-white py-3 px-12 rounded shadow font-semibold">ارسال</button>
+</div>
