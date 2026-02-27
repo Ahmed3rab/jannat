dc4587f663c9ecb409c482e89d63eb0622bba3a8
Ahmed Arab <me@ahmedarab.ly>
2026-02-28 01:05:47 +0200

    Fix rooms tab

--- resources/views/components/properties/details/rooms.blade.php
+++ resources/views/components/properties/details/rooms.blade.php
@@ -3,52 +3,35 @@
 </div>
 <dd class="mt-8 lg:px-5">
     <dl class="grid gap-10">
-        <div class="grid gap-6 lg:gap-10 grid-cols-1 lg:grid-cols-12 justify-items-start lg:items-center">
-            <dt class="lg:col-span-2 font-semibold text-primary-gray text-nowrap">
+        <div class="grid gap-6 lg:gap-10">
+            <dt class="font-semibold text-primary-gray">
                 الطابق الأرضي</dt>
-            <dd class="lg:col-span-10">
-                <dl class="grid grid-cols-3 gap-6 lg:flex lg:items-center lg:justify-center lg:gap-16">
+            <dd>
+                <dl
+                    class="grid grid-cols-3 sm:grid-cols-4 md:grid-cols-5 gap-6 sm:gap-8 lg:gap-16 xl:gap-6 2xl:gap-10 justify-items-center">
                     <div class="grid justify-items-center gap-3">
                         <dt class="flex items-center space-x-2">
-                            <svg class="size-6 text-primary-gray" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M3 21V3.6C3 3.26863 3.26863 3 3.6 3H21" stroke="#475057" stroke-width="1.5">
-                                </path>
-                                <path d="M17 21H20.4C20.7314 21 21 20.7314 21 20.4V17" stroke="#475057"
-                                    stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
-                                <path d="M21 7V9" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path d="M21 12V14" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path d="M7 21H9" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path d="M12 21H14" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path
-                                    d="M3 4C3.55228 4 4 3.55228 4 3C4 2.44772 3.55228 2 3 2C2.44772 2 2 2.44772 2 3C2 3.55228 2.44772 4 3 4Z"
-                                    fill="#475057" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path
-                                    d="M3 22C3.55228 22 4 21.5523 4 21C4 20.4477 3.55228 20 3 20C2.44772 20 2 20.4477 2 21C2 21.5523 2.44772 22 3 22Z"
-                                    fill="#475057" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
-                                <path
-                                    d="M21 4C21.5523 4 22 3.55228 22 3C22 2.44772 21.5523 2 21 2C20.4477 2 20 2.44772 20 3C20 3.55228 20.4477 4 21 4Z"
-                                    fill="#475057" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round"></path>
+
+                            <svg class="size-6 text-primary-gray" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="#475057" stroke-width="1.5" d="M3 21V3.6a.6.6 0 0 1 .6-.6H21" />
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M17 21h3.4a.6.6 0 0 0 .6-.6V17M21 7v2M21 12v2M7 21h2M12 21h2" />
+                                <path fill="#475057" stroke="#475057" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="1.5"
+                                    d="M3 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2M3 22a1 1 0 1 0 0-2 1 1 0 0 0 0 2M21 4a1 1 0 1 0 0-2 1 1 0 0 0 0 2" />
                             </svg>
                             <span class="text-primary-gray">المسقوف</span>
                         </dt>
-                        <dd class="font-semibold text-primary-gray">240 م,م</dd>
+                        <dd class="font-semibold text-primary-gray">240 م.م</dd>
                     </div>
                     <div class="grid justify-items-center gap-3">
                         <dt class="flex items-center space-x-2">
-                            <svg class="size-6 text-primary-gray" data-slot="icon" aria-hidden="true" fill="none"
-                                stroke-width="1.5" stroke="currentColor" viewBox="0 0 24 24"
-                                xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M2.25 7.125C2.25 6.504 2.754 6 3.375 6h6c.621 0 1.125.504 1.125 1.125v3.75c0 .621-.504 1.125-1.125 1.125h-6a1.125 1.125 0 0 1-1.125-1.125v-3.75ZM14.25 8.625c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v8.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-8.25ZM3.75 16.125c0-.621.504-1.125 1.125-1.125h5.25c.621 0 1.125.504 1.125 1.125v2.25c0 .621-.504 1.125-1.125 1.125h-5.25a1.125 1.125 0 0 1-1.125-1.125v-2.25Z"
-                                    stroke-linecap="round" stroke-linejoin="round"></path>
+
+                            <svg class="size-6 text-primary-gray" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="#475057" stroke-width="1.5"
+                                    d="M3 7.4V3.6a.6.6 0 0 1 .6-.6h5.8a.6.6 0 0 1 .6.6v3.8a.6.6 0 0 1-.6.6H3.6a.6.6 0 0 1-.6-.6ZM14 20.4v-3.8a.6.6 0 0 1 .6-.6h5.8a.6.6 0 0 1 .6.6v3.8a.6.6 0 0 1-.6.6h-5.8a.6.6 0 0 1-.6-.6ZM14 12.4V3.6a.6.6 0 0 1 .6-.6h5.8a.6.6 0 0 1 .6.6v8.8a.6.6 0 0 1-.6.6h-5.8a.6.6 0 0 1-.6-.6ZM3 20.4v-8.8a.6.6 0 0 1 .6-.6h5.8a.6.6 0 0 1 .6.6v8.8a.6.6 0 0 1-.6.6H3.6a.6.6 0 0 1-.6-.6Z" />
                             </svg>
                             <span class="text-primary-gray">الغرف</span>
                         </dt>
@@ -56,65 +39,40 @@
                     </div>
                     <div class="grid justify-items-center gap-3">
                         <dt class="flex items-center space-x-2">
-                            <svg class="size-6 text-primary-gray" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path
-                                    d="M21 4V20C21 21.1046 20.1046 22 19 22H5C3.89543 22 3 21.1046 3 20V4C3 2.89543 3.89543 2 5 2H19C20.1046 2 21 2.89543 21 4Z"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M3 8H11V6" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M21 8H13V6" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+                            <svg class="size-6 text-primary-gray" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M21 4v16a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h14a2 2 0 0 1 2 2" />
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M3 8h8V6M21 8h-8V6" />
                             </svg>
-
                             <span class="text-primary-gray">غرفة رئيسية</span>
                         </dt>
                         <dd class="font-semibold text-primary-gray">5</dd>
                     </div>
                     <div class="grid justify-items-center gap-3">
                         <dt class="flex items-center space-x-2">
-                            <svg class="size-6 text-primary-gray" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 16V19" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M12 13V7C12 5.89543 12.8954 5 14 5H18C19.1046 5 20 5.89543 20 7V9"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M12 13V7C12 5.89543 11.1046 5 10 5H6C4.89543 5 4 5.89543 4 7V9"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path
-                                    d="M20 9C18.8954 9 18 9.89543 18 11V13H6V11C6 9.89543 5.10457 9 4 9C2.89543 9 2 9.89543 2 11V17H22V11C22 9.89543 21.1046 9 20 9Z"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M22 16V19" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+
+                            <svg class="size-6 text-primary-gray" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M2 16v3M12 13V7a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M12 13V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2" />
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M20 9a2 2 0 0 0-2 2v2H6v-2a2 2 0 1 0-4 0v6h20v-6a2 2 0 0 0-2-2M22 16v3" />
                             </svg>
                             <span class="text-primary-gray">الصالون</span>
                         </dt>
-                        <dd class="font-semibold text-primary-gray">5</dd>
+                        <dd class="font-semibold text-primary-gray">1</dd>
                     </div>
                     <div class="grid justify-items-center gap-3">
                         <dt class="flex items-center space-x-2">
-                            <svg class="size-6 text-primary-gray" width="24" height="24" viewBox="0 0 24 24"
-                                fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M2 16V19" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M12 13V7C12 5.89543 12.8954 5 14 5H18C19.1046 5 20 5.89543 20 7V9"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M12 13V7C12 5.89543 11.1046 5 10 5H6C4.89543 5 4 5.89543 4 7V9"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path
-                                    d="M20 9C18.8954 9 18 9.89543 18 11V13H6V11C6 9.89543 5.10457 9 4 9C2.89543 9 2 9.89543 2 11V17H22V11C22 9.89543 21.1046 9 20 9Z"
-                                    stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
-                                <path d="M22 16V19" stroke="#475057" stroke-width="1.5" stroke-linecap="round"
-                                    stroke-linejoin="round" />
+                            <svg class="size-6 text-primary-gray" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                viewBox="0 0 24 24">
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M2 16v3M12 13V7a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2M12 13V7a2 2 0 0 0-2-2H6a2 2 0 0 0-2 2v2" />
+                                <path stroke="#475057" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                    d="M20 9a2 2 0 0 0-2 2v2H6v-2a2 2 0 1 0-4 0v6h20v-6a2 2 0 0 0-2-2M22 16v3" />
                             </svg>
-
                             <span class="text-primary-gray">الصالة</span>
                         </dt>
                         <dd class="font-semibold text-primary-gray">5</dd>
