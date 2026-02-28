c6ecc3329f639352ee2857ba4080a4d34a5ff3fa
Ahmed Arab <me@ahmedarab.ly>
2026-02-28 23:05:11 +0200

    Configure active nav item

--- resources/views/components/nav-link/desktop/primary.blade.php
+++ resources/views/components/nav-link/desktop/primary.blade.php
@@ -1,10 +1,17 @@
-@props(['route', 'label'])
 @php
     use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+    $isActive = request()->routeIs($route);
 @endphp
+@props([
+    'route',
+    'label',
+    'params' => [],
+    'activeClass' => 'after:scale-x-100',
+    'inactiveClass' => 'after:scale-x-0 group-hover:after:scale-x-100',
+])
 
-<a href="{{ LaravelLocalization::localizeUrl(route($route, [], false)) }}"
-    class="relative inline-block after:absolute after:-bottom-2 after:rounded-full after:left-1/2 after:-translate-x-1/2 after:w-8 after:scale-x-0 group-hover:after:scale-x-100 after:transform after:transition after:h-[0.5px] after:bg-white after:content-['']">
+<a href="{{ LaravelLocalization::localizeUrl(route($route, $params, false)) }}"
+    class="relative inline-block after:absolute after:-bottom-2 after:rounded-full after:left-1/2 after:-translate-x-1/2 after:w-8 after:transform after:transition after:h-[0.5px] after:bg-white after:content-[''] {{ $isActive ? $activeClass : $inactiveClass }}">
     <span class="relative">
         {{ __($label) }}
     </span>
--- resources/views/components/nav-link/mobile/primary.blade.php
+++ resources/views/components/nav-link/mobile/primary.blade.php
@@ -1,10 +1,17 @@
-@props(['route', 'label'])
 @php
     use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+    $isActive = request()->routeIs($route);
 @endphp
+@props([
+    'route',
+    'label',
+    'params' => [],
+    'activeClass' => 'bg-gray-100 font-semibold',
+    'inactiveClass' => 'hover:bg-gray-50 focus-visible:bg-gray-50 ',
+])
 
 <a href="{{ LaravelLocalization::localizeUrl(route($route, [], false)) }}"
-    class="px-2 lg:py-1.5 py-2 w-full flex items-center rounded-sm transition-colors text-left text-gray-800 hover:bg-gray-50 focus-visible:bg-gray-50 disabled:opacity-50 disabled:cursor-not-allowed">
+    class="px-2 lg:py-1.5 py-2 w-full flex items-center rounded-sm transition-colors text-left text-gray-800 {{ $isActive ? $activeClass : $inactiveClass }}">
     <span class="relative">
         {{ __($label) }}
     </span>
