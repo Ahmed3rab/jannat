1b5d71c046b8405c0e598b5995180c3b94b34b6b
Ahmed Arab <me@ahmedarab.ly>
2026-03-03 21:35:29 +0200

    configure subdomain for dashboard panel

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -26,7 +26,7 @@ class DashboardPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('dashboard')
-            ->path('dashboard')
+            ->domain('dashboard.' . env('APP_DOMAIN'))
             ->login()
             ->colors([
                 'primary' => Color::Amber,
